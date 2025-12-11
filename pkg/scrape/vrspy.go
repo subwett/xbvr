e04abf41e62b62ea2d38fcf91d347a7274587442
cagegine <cagegine@gmail.com>
2025-12-11 01:05:45 +0100

    update

--- pkg/scrape/vrspy.go
+++ pkg/scrape/vrspy.go
@@ -41,6 +41,7 @@ func VRSpy(wg *models.ScrapeWG, updateSite bool, knownScenes []string, out chan<
 		},
 	}
 
+	
 	sceneCollector.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36") //UserAgent)
 		for _, c := range cookies {
